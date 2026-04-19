302dc26bcad38998ed9dcd6358f57cc72fbb3b6d
agarsch <164272176+agarsch@users.noreply.github.com>
2026-04-19 13:36:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log('Meditation timer loaded');
+let time = 600;
+let interval = null;
+
+function updateDisplay() {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  document.getElementById("timer").innerText =
+    `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function startTimer() {
+  if (interval) return;
+
+  interval = setInterval(() => {
+    if (time > 0) {
+      time--;
+      updateDisplay();
+    }
+  }, 1000);
+}
+
+function pauseTimer() {
+  clearInterval(interval);
+  interval = null;
+}
+
+function resetTimer() {
+  pauseTimer();
+  time = 600;
+  updateDisplay();
+}
+
+function setTime(seconds) {
+  time = seconds;
+  updateDisplay();
+}
+
+updateDisplay();
