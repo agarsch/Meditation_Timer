842a9ac721de602dbd71de538cc84d385983828c
agarsch <164272176+agarsch@users.noreply.github.com>
2026-04-19 13:39:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // no caching for now
+});
